db5b2c327e8a20f67f53111546d35eaf156f2d37
asif <asu9421@gmail.com>
2020-05-01 13:55:43 +0530

    RestAssured jsonPath param

--- src/test/java/wiremock/TestClass1.java
+++ src/test/java/wiremock/TestClass1.java
@@ -1,16 +1,21 @@
 package wiremock;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.github.tomakehurst.wiremock.WireMockServer;
 import com.github.tomakehurst.wiremock.client.ResponseDefinitionBuilder;
 import com.github.tomakehurst.wiremock.client.WireMock;
 import com.github.tomakehurst.wiremock.http.ContentTypeHeader;
 import com.jayway.jsonpath.JsonPath;
+import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+import wiremock.myPojos.Guru;
 
 import java.io.File;
+import java.util.HashMap;
+import java.util.List;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static io.restassured.RestAssured.given;
@@ -32,11 +37,11 @@ public class TestClass1 extends MockBase {
     @Test
     public void testBasicAuth() {
         stubs.getStubForBasicAuthHeader();
-        Response response = given().
-                header("Authorization", token).
-                when().
-                get("/basic/auth/case-insensitive").
-                then().extract().response();
+        Response response = given()
+                .header("Authorization", token)
+                .when()
+                .get("/basic/auth/case-insensitive")
+                .then().extract().response();
         System.out.println(response.asString());
         Assert.assertEquals(response.getStatusCode(), 200, "Failed: Status Code didn't matched");
     }
@@ -44,12 +49,12 @@ public class TestClass1 extends MockBase {
     @Test
     public void test01_sampleGet_queryParams() {
         stubs.getStubForToolQuery(token);
-        Response response = given().
-                header("Authorization", token).
-                when().
-                queryParam("name", "Wiremock").
-                get("/tool/mocking").
-                then().extract().response();
+        Response response = given()
+                .header("Authorization", token)
+                .when()
+                .queryParam("name", "Wiremock")
+                .get("/tool/mocking")
+                .then().extract().response();
         int num = JsonPath.read(response.asString(), "$.number");
         printJson(response.asString());
         Assert.assertEquals(num, 123, "Failed: Number field mismatch");
@@ -61,12 +66,56 @@ public class TestClass1 extends MockBase {
                 .withRequestBody(matchingJsonPath("$.gurus[?(@.tool == 'Rest Assured')]"))
                 .willReturn(aResponse().withBodyFile("test02.json")));
         File file = new File("src/test/resources/__files/test02.json");
-        Response response = given().
-                when().
-                body(file).
-                post("/form/params").
-                then().extract().response();
-        System.out.println(response.asString());
+        Response response = given()
+                .when()
+                .body(file)
+                .post("/form/params")
+                .then().extract().response();
+        List<Guru> gurus = JsonPath.read(response.asString(), "$.gurus[?(@.tool =~ /^[r|R]est.*/)]");
+        System.out.println(gurus.get(0));
+    }
+
+    @Test
+    public void test03_jsonPathParamExample() {
+        stubFor(get(urlPathEqualTo("/all/gurus"))
+                .willReturn(aResponse()
+                        .withHeader(ContentTypeHeader.KEY, "application/json")
+                        .withBodyFile("test02.json")));
+
+        List<Guru> gurus01= given()
+                .when().get("/all/gurus").then()
+                .extract().jsonPath().getList("gurus",Guru.class);
+        System.out.println("Size: " + gurus01.size() + "\n" +gurus01.get(0).toString());
+
+        List<Guru> gurus02 = given()
+                .when().get("/all/gurus").then()
+                .extract().jsonPath().param("id",2).getList("gurus.findAll {it.id == id}",Guru.class);
+        System.out.println("Size: " + gurus02.size() + "\n" +gurus02.get(0).toString());
+
+        List<Guru> gurus03 = given()
+                .when().get("/all/gurus").then()
+                .extract().jsonPath().param("id",2).getList("gurus.findAll { it -> it.id == id }",Guru.class);
+        System.out.println("Size: " + gurus03.size() + "\n" +gurus03.get(0).toString());
+
+        /*Response response = given()
+                .contentType(ContentType.JSON)
+                .when()
+                .body(file)
+                .post("/form/params")
+                .then().extract().response();*/
+        //List<Guru> gurus = JsonPath.read(response.asString(), "$.gurus[?(@.tool =~ /^[r|R]est.*/)]");
+        //Guru guru1 = response.as(Guru.class);
+        //System.out.println(guru1);
+
+        /*Guru guru;
+        ObjectMapper objectMapper = new ObjectMapper();
+        ObjectReader objectReader = objectMapper.reader().forType(new TypeReference<List<MyClass>>(){});
+        List<MyClass> result = objectReader.readValue(inputStream);
+        ObjectMapper mapper = new ObjectMapper();
+
+        mapper.readValue(gurus.get(0).toString(), new TypeReference<List<Guru>>(){});
+
+        System.out.println("===============\n"+gurus1.get(0).toString()+"===============\n");*/
     }
 
     @Test
@@ -100,13 +149,14 @@ public class TestClass1 extends MockBase {
                 .withQueryParam("name", equalTo("johan-haleby"))
                 .willReturn(responseDefinitionBuilder)
         );
-        Response response = given().
-                when().
-                queryParam("name", "johan-haleby").
-                get("/getinfo/guru").
-                then().extract().response();
+        Response response = given()
+                .when()
+                .queryParam("name", "johan-haleby")
+                .get("/getinfo/guru")
+                .then().extract().response();
         System.out.println(response.asString());
     }
+
 
     @Test
     public void test04_stafeFul() {
@@ -186,4 +236,5 @@ public class TestClass1 extends MockBase {
                         .withStatus(200)
                         .withBody("Resource state")));
     }
+
 }
---
+++ src/test/java/wiremock/myPojos/Guru.java
@@ -0,0 +1,19 @@
+package wiremock.myPojos;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+
+public class Guru {
+    int id;
+    String name;
+    String company;
+    String country;
+    String tool;
+
+
+}
